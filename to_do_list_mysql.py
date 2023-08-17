1834dac5bc86e455f4dccea16d9df30a84d5930f
MAYANK KHATRI <modimayank7@gmail.com>
2023-08-17 17:23:58 +0530

    validation updated,user id taken from headers

--- to_do_list_mysql.py
+++ to_do_list_mysql.py
@@ -26,7 +26,46 @@ def db_connection():
 		print("some error occured:",error)
 
 		return jsonify(error="connection not established"),500
-    
+
+def validate_user_id(user_id):
+	
+	conn=db_connection()
+
+	cursor=conn.cursor()
+
+	query="""SELECT user_id from to_dos
+	         WHERE user_id=%s"""
+
+	cursor.execute(query,(user_id,))
+
+	result=cursor.fetchone()
+
+	if result is not None:
+		return result
+
+	else:
+		return None
+
+
+def validate_task_id(id,user_id):
+	
+	conn=db_connection()
+
+	cursor=conn.cursor()
+
+	query="""SELECT task_id from to_dos
+	         WHERE task_id=%s and user_id=%s"""
+
+	cursor.execute(query,(id,user_id))
+
+	result=cursor.fetchone()
+
+	if result is not None:
+		return result
+
+	else:
+		return None
+
 
 @app.route('/tasks/api/v1/<int:id>',methods=["GET"])
 
@@ -37,9 +76,21 @@ def get_tasks(id):
 	cur=conn.cursor()
 
 	query="""SELECT * from to_dos
-	         WHERE task_id=%s"""
+	         WHERE task_id=%s and user_id=%s"""
 
-	cur.execute(query,(id,))
+	user_id=request.headers.get("user_id")
+
+	res=validate_user_id(user_id)
+
+	if res is None:
+		return jsonify("authentication error:user id not exist"),401
+
+	res=validate_task_id(id,user_id)
+
+	if res is None:
+		return jsonify("Not Found error:task id not fond"),404
+
+	cur.execute(query,(id,user_id))
 
 	result=cur.fetchall()
 
@@ -57,7 +108,7 @@ def get_tasks(id):
 
 	conn.close()
 
-	return (jsonify(to_dos)),200
+	return jsonify(to_dos),200
 
 @app.route('/tasks/api/v1',methods=["GET"])
 
@@ -67,9 +118,18 @@ def get_all_tasks():
 
 	cur=conn.cursor()
 
-	query="""SELECT * from to_dos"""
+	query="""SELECT * from to_dos
+	         Where user_id=%s"""
 
-	cur.execute(query)
+	user_id=request.headers.get("user_id")
+
+
+	res=validate_user_id(user_id)
+
+	if not res:
+		return jsonify("authentication error:user id not exist"),401
+
+	cur.execute(query,(user_id,))
 
 	result=cur.fetchall()
 
@@ -101,24 +161,26 @@ def insert_task():
 	          (task_name,status,category,completing_date,user_id)
 	          VALUES(%s,%s,%s,%s,%s)"""
 
-
 	status_options=["due","completed"]
+
 	category_options=["finance","accounting","health","gardening","studying"]
 
+	user_id=request.headers.get("user_id")
 	task_name=request.form["task_name"]
 	status=request.form["status"]
 	category=request.form["category"]
 	completing_date=request.form["completing_date"]
-	user_id=request.form["user_id"]
-
-    
 
 	if status not in status_options:
 		return jsonify("this status option not avaliable,please enter valid status in form options avaliable : due , completed"),400
 
-	for item in (category.split(",")) :
-		if item not in category_options:
-			return jsonify("this category option not avaliable,please enter valid category in form options avaliable"),400               
+	if len(category.split(","))!=0:
+		for item in (category.split(",")) :
+			if item not in category_options:
+				return jsonify("this category option not avaliable,please enter valid category in form options avaliable"),400               
+
+	else:
+		return jsonify("this category option not avaliable,please enter valid category in form options avaliable"),400
 
 	cursor.execute(query,(task_name,status,category,completing_date,user_id))
 
@@ -146,19 +208,60 @@ def delete_task(id):
 
 	cursor=conn.cursor()
 
-	query="""SELECT task_id from to_dos
-	         where task_id=%s"""
+	user_id=request.headers.get("user_id")
+
+	res=validate_user_id(user_id)
+
+	if not user_id:
+		return jsonify("authentication error:user_id not exists"),401
+
+	res=validate_task_id(id,user_id)
+
+	if not res: 
+		return jsonify("data not found:for given user id,task id not found"),404
+
+
+	query="""DELETE FROM to_dos
+		     Where task_id=%s and user_id=%s"""
+
+	cursor.execute(query,(id,user_id))
+
+	conn.commit()
+
+	cursor.close()
+
+	conn.close()
+
+	return jsonify("task with task_id {} for user_id {} deleted from to_do_list ".format(id,user_id)),201
+
+@app.route("/tasks/api/v1/<int:id>",methods=["PATCH"])
+
+def update_task(id):
+
+	conn=db_connection()
+
+	cursor=conn.cursor()
+
+	user_id=request.headers.get("user_id")
     
-	cursor.execute(query,(id,))
+	res=validate_user_id(user_id)
 
-	result=cursor.fetchone()
+	if not res:
+		return jsonify("authentication error:user_id not exists"),401
 
-	if result is not None:
+	res=validate_task_id(id,user_id)
 
-		query="""DELETE FROM to_dos
-		         where task_id=%s"""
+	if not res:
+		return jsonify("data not found:for given user id,task id not found"),404
 
-		cursor.execute(query,(id,))
+	task_name=request.form.get("task_name",default="")
+
+	if task_name:
+		query="""UPDATE to_dos SET 
+		         task_name=%s
+		         WHERE user_id=%s and task_id=%s"""
+
+		cursor.execute(query,(task_name,user_id,id))
 
 		conn.commit()
 
@@ -166,85 +269,24 @@ def delete_task(id):
 
 		conn.close()
 
-		return jsonify("task with task_id {} deleted from to_do_list ".format(id)),200
+		return jsonify("task updated:task_name for given task_id and user_id updated"),201
 
-	cursor.close()
+		status=request.form.get("status",default="")
 
-	conn.close()
+		if status:
+			query="""UPDATE to_dos SET 
+			         status=%s
+			         WHERE user_id=%s and task_id=%s"""
 
-	return jsonify("task with task_id doesn't exist in your to_do_list"),404
+			cursor.execute(query,(status,user_id,id))
 
-@app.route("/tasks/api/v1/<int:usr_id>/<int:id>",methods=["PATCH"])
+			conn.commit()
 
-def update_task(user_id,id):
+			cursor.close()
 
-	conn=db_connection()
+			conn.close()
 
-	cursor=conn.cursor()
-
-	query="""SELECT user_id from to_dos
-	         where user_id=%s"""
-
-	cursor.execute(query,(usr_id,))
-
-	result=cursor.fetchone()
-
-	if result is not None:
-
-		query="""SELECT task_id from to_dos
-		         where task_id=%s and user_id=%s"""
-
-		cursor.execute(query,(id,usr_id))
-
-		result=cursor.fetchone()
-
-		if result is not None:
-
-			task_name=request.form.get("task_name",default="")
-
-			if task_name:
-
-				query="""UPDATE to_dos SET 
-				         task_name=%s
-				         WHERE user_id=%s and task_id=%s"""
-
-
-				cursor.execute(query,(task_name,usr_id,id))
-
-				conn.commit()
-
-				cursor.close()
-
-				conn.close()
-
-				return "task updated"
-
-			status=request.form.get("status",default="")
-
-			if status:
-
-				query="""UPDATE to_dos SET 
-				         status=%s
-				         WHERE user_id=%s and task_id=%s"""
-
-
-				cursor.execute(query,(status,usr_id,id))
-
-				conn.commit()
-
-				cursor.close()
-
-				conn.close()
-
-				return "task updated"
-
-			else:
-				return "please enter valid task"
-		else:
-			return "task id for user_id {} doesn't exist".format(usr_id)
-
-	else:
-		return "user id {} not exist".format(usr_id)
+			return jsonify("task updated:status for given task_id and user_id updated"),201
 
 @app.route('/users/api/v1',methods=["GET"])
 
@@ -271,7 +313,7 @@ def get_user_info():
 
 	conn.close()
 
-	return jsonify(user_information)
+	return jsonify(user_information),200
 
 @app.route("/users/api/v1",methods=["POST"])
 
@@ -303,7 +345,7 @@ def insert_user_info():
 
 	conn.close()
 
-	return jsonify(user_list)
+	return jsonify(user_list),201
 
 @app.route("/users/api/v1/<int:usr_id>",methods=["PATCH"])
 
@@ -317,11 +359,21 @@ def user_info_update(usr_id):
 	         SET address=%s
 	         WHERE user_id=%s"""
 
-	user_address=request.form["address"]
+	user_address=request.form.get("address",default="")
 
 	user_id=usr_id
 
-	cur.execute(query,(user_address,user_id))
+	query="""SELECT user_id from users
+	         WHERE user_id=%s"""
+
+	cur.execute(query,(usr_id))
+
+	res=cur.fetchone()
+
+	if not res:
+		return jsonify("not found error:user id not found"),404
+
+	cur.execute(query,(address,user_id))
 
 	conn.commit()
 
@@ -329,8 +381,7 @@ def user_info_update(usr_id):
 
 	conn.close()
 
-	return "user_info updated"
-
+	return jsonify("user_info updated:address updated"),201
 
 
 if __name__=="__main__":
